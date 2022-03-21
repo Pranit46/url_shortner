e926e84799054a8dc22f3d981c920854f901dd89
Pranit Yadav <42487480+Pranit46@users.noreply.github.com>
2022-03-21 12:46:14 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,5 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 dbName = "SHORTURL_USER_DATABASEN1"
-const dbUrl = `mongodb+srv://admin:l96gYJFQpGVs9ATC@pranit7.yhko9.mongodb.net/${dbName}`;
+const dbUrl = `mongodb+srv://admin:<password>@pranit7.yhko9.mongodb.net/${dbName}`;
 module.exports = { dbUrl, mongodb, MongoClient };
